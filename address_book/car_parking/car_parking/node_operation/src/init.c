73b8f0b444b6feea8c9bf766924fdbc6d8625e77
fengguang <1143955880@qq.com>
2014-12-18 20:03:43 +0800

    CC

--- address_book/car_parking/car_parking/node_operation/src/init.c
+++
@@ -1,9 +0,0 @@
-#include "../../include/myhead.h"
-
-void init(Link *head)
-{    
-    *head = (Link)malloc(sizeof(Car));
-    (*head)->next = NULL;
-    (*head)->prior = NULL;
-    top = *head;       
-}
